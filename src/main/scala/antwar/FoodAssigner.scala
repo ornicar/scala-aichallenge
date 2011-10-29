a44d64051130a4db9f46ce8353e81307c1d3444a
ornicar <thibault.duplessis@gmail.com>
2011-10-29 16:41:32 +0200

    Implement food assignement with a backtrack recursive algorithm

--- src/main/scala/antwar/FoodAssigner.scala
+++ src/main/scala/antwar/FoodAssigner.scala
@@ -1,5 +1,7 @@
 package antwar
 
+import scala.math.max
+
 object FoodAssigner {
 
   //(A, 5) (B, 7) (C, 9)
@@ -12,10 +14,10 @@ object FoodAssigner {
   //B = 5
   //-- = 99
 
-  //A, B, C
-  //A, D
-  //B
-  //B
+  //A, B, C, -
+  //A, D, -
+  //B, -
+  //B, -
 
   //A D B -
   //B A - -
@@ -24,9 +26,41 @@ object FoodAssigner {
   //C D B -
   //C D - B
 
-  def assign[Ant](foodsAntsDist: List[Map[Ant, Int]]): List[Ant] = {
-    val foodAnts: List[List[Ant]] = foodsAntsDist map (_._1)
-    val ants: List[Ant] = foodAnts flatMap identity
+  def assign[Ant](foodsAntsDist: List[Map[Ant, Int]]): List[Option[Ant]] = {
+    val foodsAnts: List[List[Ant]] = foodsAntsDist map { as => (as map (_._1)).toList }
+    val foodsAntOptions: List[List[Option[Ant]]] = foodsAnts map { fas =>
+      None :: (fas map { Some(_) })
+    }
+
+    def assignements(fas: List[List[Option[Ant]]]): List[List[Option[Ant]]] = {
+      fas match {
+        case Nil => List(Nil)
+        case foodAnts :: rest => {
+          for {
+            solution <- assignements(rest)
+            candidate <- foodAnts
+            if (candidate.isEmpty || !(solution contains candidate))
+          } yield candidate :: solution
+        }
+      }
+    }
+    val solutions = assignements(foodsAntOptions)
+
+    //val visual = (solutions map { _ map {
+      //case None => "-"
+      //case Some(x) => x.toString
+    //} mkString "" }) mkString "\n"
+    //println(visual)
+
+    def cost(solution: List[Option[Ant]], dists: List[Map[Ant, Int]]): Int = solution match {
+      case Nil => 0
+      case ant :: rest => cost(rest, dists.tail) + (ant match {
+        case None =>  999
+        case Some(a) => dists.head(a)
+      })
+    }
+
+    solutions minBy { cost(_, foodsAntsDist) }
   }
 
 }
