b9d0a5df3037d137acae93b41eb28d797ab8401c
Thibault Duplessis <thibault.duplessis@gmail.com>
2011-11-06 16:56:40 +0100

    Implement oneliner side effects

--- src/main/scala/antwar/package.scala
+++ src/main/scala/antwar/package.scala
@@ -3,4 +3,19 @@ import antwar.foundation._
 package object antwar {
 
   implicit def positionable2Tile(positionable: Positionable): Tile = positionable.tile
+
+  /**
+   * Provides oneliner side effects
+   * See http://hacking-scala.posterous.com/side-effecting-without-braces
+   */
+  implicit def anyToSideEffect[A](any: A) = new {
+    def ~(sideEffect: (=>A) => Unit): A = {
+      sideEffect(any)
+      any
+    }
+    def ~(title: String): A = {
+      Logger(title)(any)
+      any
+    }
+  }
 }
